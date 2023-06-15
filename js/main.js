6e2dcc4ba29b1a6d835f0af335eae762b0d6c6bf
niels <it@bollaert.email>
2023-06-15 17:53:49 +0200

    Refactoring js, moving styles to css sheet instead of html

--- js/main.js
+++ js/main.js
@@ -5,123 +5,141 @@
 			sheet;
 		document.head.appendChild(element);
 		sheet = element.sheet;
-		let cs = "#genesis-content > div.woocommerce-notices-wrapper { display: none; }";
+		let cs = "div.woocommerce-notices-wrapper { display: none; }";
 		sheet.insertRule(cs, 0);
 	}
 
-	/* UI, open pop-up bootstrap */
-	function openModal(products, upsells) {
-		var body =
-			'<div class="" style="padding-left: 20px; padding-right: 20px;">' +
-			'<div class="row" style="clear:both; margin-bottom:15px; margin-top: 15px;">' +
-			'<div class="col-md-12 cart-header-container " style="padding: 0 !important;">' +
-			'<h4 class="winkelwagen-popup-h4">Toegevoegd aan je winkelmand</h4>' +
-			'<button class="winkelwagen-popup-buttonverderwinkelen" data-bs-dismiss="modal" aria-label="Close"><i class="fa fa-times"> <span>Verder winkelen</span></i></button>' +
-			"</div>" +
-			"</div>";
+	class PopupBuilder {
+		constructor() {
+			this.html = "";
+		}
 
-		product = products.shift();
+		buildBeginWrapper() {
+			this.html += '<div id="popup-wrapper">';
+		}
 
-		body +=
-			'<div class="row" style="display: flex; justify-content: center; clear:both; border-bottom: 1px solid #e6e6e6 !important; border-top: 1px solid #e6e6e6 !important; margin-bottom: 20px; padding-top: 20px; padding-bottom: 20px;">' +
-			'<div class="col-lg-2 col-md-6 col-sm-12" style="align-items: center;display: flex;justify-content: center;">' +
-			'<img src="' +
-			product["image"] +
-			'" width="100px"></img>' +
-			"</div>" +
-			'<div class="col-lg-3 col-md-6 col-sm-12 name-container" style="align-items: center;display: flex;justify-content: center;">' +
-			'<span class="winkelwagen-popup-name">' +
-			product["name"] +
-			"</span>" +
-			"</div>" +
-			'<div class="col-lg-1 col-md-6 col-sm-12 quantity-container" style="align-items: center;display: flex;justify-content: center;">' +
-			'<span class="winkelwagen-popup-name">x' +
-			product["quantity"] +
-			"</span>" +
-			"</div>" +
-			'<div class="col-lg-3 col-md-6 col-sm-12 price-container" style="align-items: center;display: flex;justify-content: center; flex-direction: column;">' +
-			'<div class="winkelwagen-popup-price ex-btw"><span class="headProductPrice">€ ' +
-			Number(product["price"] * product["quantity"]).toLocaleString("nl-BE", {
-				minimumFractionDigits: 2,
-				maximumFractionDigits: 2,
-			}) +
-			"</span> <span class='headProduct-ex-btw'>Excl. btw</span></div>" +
-			'<div class="winkelwagen-popup-price inc-btw"><span>€ ' +
-			Number(product["price"] * product["quantity"] * 1.21).toLocaleString("nl-BE", {
-				minimumFractionDigits: 2,
-				maximumFractionDigits: 2,
-			}) +
-			"</span> <span class='headProduct-inc-btw'>Incl. btw</span></div>" +
-			"</div>" +
-			'<div class="winkelwagen-popup-buttondiv col-lg-3 col-md-6 col-sm-12"> ' +
-			'<button class="button-winkelmand"><a id="winkelwagenpopup-button-link" href="/winkelmand">Winkelmand</a><i class="fa fa-arrow-right"></i></button>' +
-			"</div>" +
-			"</div>";
+		buildEndWrapper() {
+			this.html += "</div>";
+		}
 
-		products.forEach((product) => {
-			body +=
-				'<div class="row" style="display: flex; justify-content: center; clear:both; border-bottom: 1px solid #e6e6e6 !important; border-top: 1px solid #e6e6e6 !important; margin-bottom: 20px; padding-top: 20px; padding-bottom: 20px;">' +
-				'<div class="col-lg-2 col-md-6 col-sm-12" style="align-items: center;display: flex;justify-content: center;">' +
+		buildHeader(title) {
+			this.html +=
+				'<div class="row cart-header-wrapper">' +
+				'<div class="col-md-12 cart-header-container">' +
+				'<h4 class="winkelwagen-popup-h4">Toegevoegd aan je winkelmand</h4>' +
+				'<button class="winkelwagen-popup-buttonverderwinkelen" data-bs-dismiss="modal" aria-label="Close"><i class="fa fa-times"> <span>Verder winkelen</span></i></button>' +
+				"</div>" +
+				"</div>";
+		}
+
+		buildHeadProduct(product) {
+			this.html +=
+				'<div class="row head-product-wrapper">' +
+				'<div class="col-lg-2 col-md-6 col-sm-12 image-container">' +
 				'<img src="' +
 				product["image"] +
 				'" width="100px"></img>' +
 				"</div>" +
-				'<div class="col-lg-3 col-md-6 col-sm-12 other-name-container" style="align-items: center;display: flex;justify-content: center;">' +
+				'<div class="col-lg-3 col-md-6 col-sm-12 name-container">' +
 				'<span class="winkelwagen-popup-name">' +
 				product["name"] +
 				"</span>" +
 				"</div>" +
-				'<div class="col-lg-1 col-md-6 col-sm-12 other-quantity-container" style="align-items: center;display: flex;justify-content: center;">' +
+				'<div class="col-lg-1 col-md-6 col-sm-12 quantity-container">' +
 				'<span class="winkelwagen-popup-name">x' +
 				product["quantity"] +
 				"</span>" +
 				"</div>" +
-				'<div class="col-lg-3 col-md-6 col-sm-12 other-price-container" style="align-items: center;display: flex;justify-content: center; flex-direction: column;">' +
-				'<div class="winkelwagen-popup-price ex-btw"><span class="otherProductPrice">€ ' +
+				'<div class="col-lg-3 col-md-6 col-sm-12 price-container" style="">' +
+				'<div class="winkelwagen-popup-price ex-btw"><span class="headProductPrice">€ ' +
 				Number(product["price"] * product["quantity"]).toLocaleString("nl-BE", {
 					minimumFractionDigits: 2,
 					maximumFractionDigits: 2,
 				}) +
-				"</span> <span class='otherProduct-ex-btw'>Excl. btw</span></div>" +
+				"</span> <span class='headProduct-ex-btw'>Excl. btw</span></div>" +
 				'<div class="winkelwagen-popup-price inc-btw"><span>€ ' +
 				Number(product["price"] * product["quantity"] * 1.21).toLocaleString("nl-BE", {
 					minimumFractionDigits: 2,
 					maximumFractionDigits: 2,
 				}) +
-				"</span> <span class='otherProduct-inc-btw'>Incl. btw</span></div>" +
+				"</span> <span class='headProduct-inc-btw'>Incl. btw</span></div>" +
 				"</div>" +
 				'<div class="winkelwagen-popup-buttondiv col-lg-3 col-md-6 col-sm-12"> ' +
+				'<button class="button-winkelmand"><a id="winkelwagenpopup-button-link" href="/winkelmand">Winkelmand</a><i class="fa fa-arrow-right"></i></button>' +
 				"</div>" +
 				"</div>";
-		});
-		if (upsells && upsells.length > 0) {
-			body +=
-				'<div class="row" style="margin-bottom: 15px;"><div class="col-md-12" style="text-align:center;"><h5 class="winkelwagen-popup-h5">Ook handig om gelijk mee te bestellen!</h5></div></div>' +
+		}
+
+		buildGroupedProducts(products) {
+			products.forEach((product) => {
+				this.html +=
+					'<div class="row other-product-wrapper">' +
+					'<div class="col-lg-2 col-md-6 col-sm-12 other-image-container">' +
+					'<img src="' +
+					product["image"] +
+					'" width="100px"></img>' +
+					"</div>" +
+					'<div class="col-lg-3 col-md-6 col-sm-12 other-name-container">' +
+					'<span class="winkelwagen-popup-name">' +
+					product["name"] +
+					"</span>" +
+					"</div>" +
+					'<div class="col-lg-1 col-md-6 col-sm-12 other-quantity-container">' +
+					'<span class="winkelwagen-popup-name">x' +
+					product["quantity"] +
+					"</span>" +
+					"</div>" +
+					'<div class="col-lg-3 col-md-6 col-sm-12 other-price-container">' +
+					'<div class="winkelwagen-popup-price ex-btw"><span class="otherProductPrice">€ ' +
+					Number(product["price"] * product["quantity"]).toLocaleString("nl-BE", {
+						minimumFractionDigits: 2,
+						maximumFractionDigits: 2,
+					}) +
+					"</span> <span class='otherProduct-ex-btw'>Excl. btw</span></div>" +
+					'<div class="winkelwagen-popup-price inc-btw"><span>€ ' +
+					Number(product["price"] * product["quantity"] * 1.21).toLocaleString("nl-BE", {
+						minimumFractionDigits: 2,
+						maximumFractionDigits: 2,
+					}) +
+					"</span> <span class='otherProduct-inc-btw'>Incl. btw</span></div>" +
+					"</div>" +
+					'<div class="winkelwagen-popup-buttondiv col-lg-3 col-md-6 col-sm-12"> ' +
+					"</div>" +
+					"</div>";
+			});
+		}
+
+		buildUpsellTitle() {
+			this.html +=
+				'<div class="upsell-title-wrapper"><div class="col-md-12" style="text-align:center;"><h5 class="winkelwagen-popup-title">Ook handig om gelijk mee te bestellen!</h5></div></div>' +
 				'<div class="products row" style="align-items: center; justify-content: center;">';
+		}
+
+		buildUpsells(upsells) {
 			upsells.forEach((upsell) => {
-				body +=
-					'<div class="row" style="clear:both; border-bottom: 1px solid #e6e6e6 !important; border-top: 1px solid #e6e6e6 !important; margin-bottom: 20px; padding-top: 20px; padding-bottom: 20px;">' +
-					'<div class="col-lg-3 col-md-6 col-sm-12" style="align-items: center;display: flex;justify-content: center;">' +
+				this.html +=
+					'<div class="row upsell-wrapper">' +
+					'<div class="col-lg-3 col-md-6 col-sm-12 upsell-image-container">' +
 					'<img src="' +
 					upsell["image"] +
 					'" width="100px"></img>' +
 					"</div>" +
-					'<div class="col-lg-3 col-md-6 col-sm-12 upsell-name-container" style="align-items: center;display: flex;justify-content: center;">' +
+					'<div class="col-lg-3 col-md-6 col-sm-12 upsell-name-container">' +
 					'<span class="winkelwagen-popup-name"><a href="' +
 					upsell["link"] +
 					'" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">' +
 					upsell["name"] +
 					"</a></span>" +
 					"</div>" +
-					'<div class="col-lg-3 col-md-6 col-sm-12 upsell-price-container" style="align-items: center;display: flex;justify-content: center; flex-direction: column;">' +
+					'<div class="col-lg-3 col-md-6 col-sm-12 upsell-price-container">' +
 					'<div class="winkelwagen-popup-price ex-btw"><span class="upsellProductPrice">€ ' +
-					Number(product["price"] * product["quantity"]).toLocaleString("nl-BE", {
+					Number(upsell["price"] * 1).toLocaleString("nl-BE", {
 						minimumFractionDigits: 2,
 						maximumFractionDigits: 2,
 					}) +
 					"</span> <span class='upsellProduct-ex-btw'>Excl. btw</span></div>" +
 					'<div class="winkelwagen-popup-price inc-btw"><span>€ ' +
-					Number(product["price"] * product["quantity"] * 1.21).toLocaleString("nl-BE", {
+					Number(upsell["price"] * 1 * 1.21).toLocaleString("nl-BE", {
 						minimumFractionDigits: 2,
 						maximumFractionDigits: 2,
 					}) +
@@ -132,16 +150,34 @@
 					upsell["id"] +
 					'" data-open="0" data-quantity="1" class="button wp-element-button product_type_simple ajax_add_to_cart add_to_cart_button" data-product_id="' +
 					upsell["id"] +
-					'" data-product_sku="" aria-label="Toevoegen" rel="nofollow">Toevoegen</a></button>' +
+					'" aria-label="Toevoegen" rel="nofollow">Toevoegen</a></button>' +
 					"</div>" +
 					"</div>";
 			});
-			body += "</div>";
 		}
+
+		build(products, upsells) {
+			this.html = "";
+			this.buildBeginWrapper();
+			this.buildHeader();
+			this.buildHeadProduct(products.shift());
+			this.buildGroupedProducts(products);
+			this.buildUpsellTitle();
+			this.buildUpsells(upsells);
+			this.buildEndWrapper();
+			return this.html;
+		}
+	}
+
+	/* UI, open pop-up bootstrap */
+	function openModal(products, upsells) {
+		const body = new PopupBuilder();
+		var html = body.build(products, upsells);
+
 		bootstrap.showModal({
 			title: "winkelmand",
 			backdrop: true,
-			body,
+			body: html,
 			footer:
 				'<button type="submit" class="btn btn-primary save-btn" style="background-color: #1D97FF;">' +
 				"save" +
@@ -149,7 +185,9 @@
 			onCreate: function (modal) {},
 		});
 	}
+	//emd class
 
+	//Wait until an element exists
 	function waitForElm(selector) {
 		return new Promise((resolve) => {
 			if (document.querySelector(selector)) {
@@ -170,7 +208,6 @@
 		});
 	}
 
-	/* Triggers */
 	/* After DOM is ready */
 	$(document).ready(function () {
 		$(document).on("click", ".winkelwagen-popup-button", function () {
@@ -181,13 +218,14 @@
 			});
 		});
 
-		/* Open pop-up if blue banner appears in dom */
+		/* Open pop-up normal wp banner appears in dom */
 		if ($(".woocommerce-message > a").length && is_product_page) {
 			openModal(products, upsells);
 			$(".modal-dialog").addClass("modal-lg");
 			$(".modal-header").remove();
 			$(".modal-footer").remove();
 			var btn = $(this);
+			//Clear session data after modal opens
 			$.ajax({
 				url: ajax_object.ajaxurl,
 				type: "POST",
@@ -200,6 +238,7 @@
 			});
 		}
 
+		//Clear session data when modal is closed
 		$("body").on("hidden.bs.modal", ".modal", function () {
 			$.ajax({
 				url: ajax_object.ajaxurl,
@@ -211,12 +250,13 @@
 			});
 		});
 
+		// Close pop-up when clicking outside of it
 		$("body").on("click", ".modal", function (ev) {
 			if (ev.target != $(this)[0]) return;
 			$(".modal").modal("hide");
 		});
 
-		/* Open pop-up on category page */
+		// Open pop-up on archive page via ajax
 		$(".ajax_add_to_cart").click(function (ev) {
 			if ($(this).data("open") != undefined) return;
 
